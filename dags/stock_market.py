4f9e95dcf95fc4f350df3c425088cfa9737ea8af
pareshkubernetes <patelpareshr@yahoo.com>
2026-04-04 20:41:16 -0500

    gitignore

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -2,7 +2,7 @@ from airflow.decorators import dag
 from datetime import datetime
 
 @dag(
-    start_date=datetime(2024, 01, 01),
+    start_date=datetime(2024, 1, 1),
     schedule='@daily',
     catchup=False,
     tags=['stock_market']
